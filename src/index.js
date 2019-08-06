3fc8b0a46da5d967a6db711994cf9b5a59961a9d
github username <github email address>
2019-08-06 17:05:41 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
 });
+
+
+
+form = document.querySelector("form")
+form.addEventListener("submit", submitHandle)
+
+array = []
+function submitHandle(e){
+  e.preventDefault()
+  let td = document.querySelector("#new-task-description").value
+  let list = document.getElementById("tasks")
+  let listItem = document.createElement("li")
+  listItem.innerHTML = td
+  list.appendChild(listItem)
+  let deleteItem = document.createElement("BUTTON")
+  listItem.appendChild(deleteItem)
+  // deleteItem.addEventListener("click", function(){
+  //   list.removeChild(listItem)
+  // })
+  deleteItem.addEventListener("click", function(){
+    listItem.remove()
+  })
+    let ps = document.querySelector("#prioritySelector").value
+    listItem.dataset.priority = ps 
+    array.push({item: listItem, priority: ps})
+    array.sort(function(a, b){return a.priority - b.priority})
+    debugger
+    switch (ps){
+    case "1":  
+    listItem.style.color = "red"
+    break
+    case "2":   
+    listItem.style.color = "yellow"
+    break
+    case "3":
+      listItem.style.color = "green"
+      break
+    }
+    
+ 
+  
+
+
+
+}
