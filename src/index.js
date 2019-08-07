d9e641cfc797493474372118aab8458e3489f021
lesliesage <leslie.sage@gmail.com>
2019-08-07 11:02:57 -0400

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -5,34 +5,34 @@ document.addEventListener("DOMContentLoaded", () => {
   const priority = ["red","yellow","green"];
   const priorityHash = {red: 0, yellow: 0, green: 0};
   let newTaskText = "";
+  let newTaskColor = "";
   let insertNum = 0;
-  let insertSpot;
   let taskToDelete;
-  let newTaskColor;
 
   formEl.addEventListener('submit', function(e){
+    // Make sure submit doesn't reload page
     e.preventDefault();
-    // newTaskText = (e.target[0].value);   OR next line
+    // Make new task element
+        // newTaskText = (e.target[0].value); <- replaced this with next line
     newTaskText = (inputEl.value);
     newTask = document.createElement("li");
-    // todoEl.appendChild(newTask);  // comment out when sort works?
     newTask.innerText = newTaskText;
-
+    // Color task element by priority
     priorityChoice = e.target[0].form[1].options.selectedIndex;
     newTaskColor = priority[priorityChoice];
     newTask.style.backgroundColor = newTaskColor;
-
+    // Insert task into list in order of priority
     if (newTaskColor === "green" ){
       todoEl.appendChild(newTask);
     } else if (newTaskColor === "yellow" ) {
       insertNum = priorityHash["red"]+priorityHash["yellow"];
-      todoEl.insertBefore(newTask, todoEl.children[insertNum])
+      todoEl.insertBefore(newTask, todoEl.children[insertNum]);
     } else {
       insertNum = priorityHash["red"];
-      todoEl.insertBefore(newTask, todoEl.children[insertNum])
+      todoEl.insertBefore(newTask, todoEl.children[insertNum]);
     };
-    priorityHash[newTaskColor]++;
-
+    priorityHash[newTaskColor]++; // Update tracker of color counts
+    // Add delete button to task
     newDeleteButton = document.createElement("button");
     newDeleteButton.innerText = "X";
     newTask.appendChild(newDeleteButton);
@@ -41,7 +41,6 @@ document.addEventListener("DOMContentLoaded", () => {
     newDeleteButton.addEventListener('click', function(e){
       taskToDelete = e.target.parentElement;
       taskToDelete.remove();
-      });
-    }
-  );
+    });
+  });
 });
